58ca16d8f0d5f08fd086b86265023748915217e1
1ritika <pritika351@gmail.com>
2022-08-25 15:06:40 +0530

    appspec

--- src/main/java/com/roms/packagingdelivery/controller/PackagingDeliveryController.java
+++ src/main/java/com/roms/packagingdelivery/controller/PackagingDeliveryController.java
@@ -2,6 +2,7 @@ package com.roms.packagingdelivery.controller;
 
 import com.roms.packagingdelivery.service.PackagingAndDeliveryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@RequestMapping("/packagingAndDelivery")
+@CrossOrigin(origins={"http://localhost:8081","http://localhost:4200"})
 public class PackagingDeliveryController {
 
     public PackagingAndDeliveryService packagingAndDeliveryService;
